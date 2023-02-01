69d616cad8d9853993a708e79a373530e73886d3
Juan Rolo <juanluis211@gmail.com>
2023-02-01 23:45:31 +0100

    Change src of images in header and NoMarch

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
       <nav className="navbar navbar-expand-md">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
-            <img src="/src/assets/logo/logotipo-yustidesigner-04.svg" alt="logo" />
+            <img src="../../assets/logo/logotipo-yustidesigner-04.svg" alt="logo" />
           </a>
           <button
             className="navbar-toggler"
--- src/pages/NoMatch/NoMatch.jsx
+++ src/pages/NoMatch/NoMatch.jsx
@@ -4,7 +4,7 @@ import './NoMatch.scss'
 function NoMatch() {
   return (
     <div className='container text-center noMatch'>
-      <img src="/src/assets/imgs/ghost.png" alt="Under construction" width="300" />
+      <img src="../../assets/imgs/ghost.png" alt="Under construction" width="300" />
       <h3 className='mt-5 noMatch__title'>Página en construcción</h3>
       <p className='noMatch__subtitle'>Estamos dando lo mejor por crear una web digna para tí, pronto estará lista, mientras tanto te dejo este formulario de contacto donde puedes consultar lo que desees y te responderé en la brevedad posible.</p>
     </div>
